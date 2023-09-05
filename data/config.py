ee9429aa62a654edac5485dc891922ee24b71005
Alex Novsky <68556113+AlexNovsky@users.noreply.github.com>
2023-09-05 17:13:29 -0500

    PyCharm bug forces to sync with git to prevent code loss

---
+++ data/config.py
@@ -0,0 +1,31 @@
+search_level = 3
+connection_level = 2
+per_company_limit = 10
+driver_wait = 20
+headless = True
+search_list = [
+    "SpaceX",
+    "NASA",
+    "SpinLaunch",
+    "Sierra Space",
+    "Relativity Space",
+    "Rocket Lab",
+    "Blue Origin",
+    "Tesla",
+    "ARRIVAL",
+    "Zoox",
+    "Rivian",
+    "Redwood Materials",
+    "Neuralink",
+    "Boring Company",
+    "OpenAI",
+    "DeepMind",
+    "Boston Dynamics",
+    "Waymo",
+]
+job_titles = [
+    "Recruiter",
+    "Talent",
+    "Software",
+    "SDET"
+]
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from tkinter import *
 from tkinter import messagebox
+import os
+
+# Check if the 'data' directory exists
+if not os.path.exists('data'):
+    os.makedirs('data')
+# Check if the 'credentials.py' file exists
+if not os.path.isfile('data/credentials.py'):
+    # If it doesn't exist, create it with default values
+    with open('data/credentials.py', 'w', encoding='utf8') as outfile:
+        outfile.write('email = "your_default_email"\n')
+        outfile.write('password = "your_default_password"\n')
+
+from data import credentials
+from data import config
 
 # ==================== Variables ====================
 white = '#FFFFFF'
 text_blue = '#01649b'
-
+clear_btn_img = None
+save_btn_img = None
+connect_btn_img = None
 # ==================== Data operations ====================
 def clear(entry) -> None:
     """
@@ -21,10 +37,18 @@ def clear_cred() -> None:
     """
     clear(login_entry)
     clear(pwd_entry)
+    # clear(job_title_entry)
+    # clear(company_entry)
+    login_entry.focus()
+
+def clear_params() -> None:
+    """
+    Remove any text in the website and password text boxes
+    :return:            None
+    """
     clear(job_title_entry)
     clear(company_entry)
-
-    login_entry.focus()
+    job_title_entry.focus()
 
 def get_value(entry) -> str:
     """
@@ -34,50 +58,191 @@ def get_value(entry) -> str:
     """
     value = entry.get()
     return value
-def empty_field_check() -> bool:
+
+def empty_cred_field_check():
     """
     Checking that all fields are filled with information and passing to the data saving
     :return:            None
     """
     login = get_value(login_entry)
     pwd = get_value(pwd_entry)
-    title = get_value(job_title_entry)
-    company = get_value(company_entry)
+    # title = get_value(job_title_entry)
+    # company = get_value(company_entry)
 
     # self.set_vars()
-    if len(login) == 0 or len(pwd) == 0 or len(title) == 0 or len(company) == 0:
+    if len(login) == 0 or len(pwd) == 0:
         messagebox.showinfo(title='Empty fields', message='Please, do not leave any fields empty')
+        return False
     else:
         return True
 
+def empty_param_field_check():
+    """
+    Checking that all fields are filled with information and passing to the data saving
+    :return:            None
+    """
+    job_title = get_value(job_title_entry)
+    search_list = get_value(company_entry)
+
+    # self.set_vars()
+    if len(job_title) == 0 or len(search_list) == 0:
+        messagebox.showinfo(title='Empty fields', message='Please, do not leave any fields empty')
+        return False
+    else:
+        return True
+
+
+def get_cred():
+    try:
+        with open('data/credentials.py', 'r', encoding='utf8') as outfile:
+            data = {
+            "email": credentials.email,
+            "password": credentials.password,
+            }
+        return data
+    except FileNotFoundError:
+        with open('data/credentials.py', 'w', encoding='utf8') as outfile:
+            outfile.write('email = "your_default_email"\n')
+            outfile.write('password = "your_default_password"\n')
+            data = {
+                "email": credentials.email,
+                "password": credentials.password,
+            }
+            return data
+    finally:
+        pass
+
 def predefine_cred():
+    global login_entry, pwd_entry
     """
     Check if data file with login and password exist and populating values at the application start.
     If file doesn't exist (first run) - returning None
     :return:            None
     """
-    # self.set_vars()
-    try:
-        with open('data/credentials.py', 'r', encoding='utf8') as outfile:
-            # read data from file in format:
-    # email = "value"
-    # password = "value"
-            # data = json.load(outfile)
-    except FileNotFoundError:
-        messagebox.showinfo(title='No file found', message='Looks like you are running this program first time. \n'
-                                                           'Data with password records not found')
+    cred = get_cred()
+    if len(cred['email']) == 0 and len(cred['password']) == 0:
+        login_entry.insert(0, 'your_default_email')
+        pwd_entry.insert(0, 'your_default_password')
     else:
-        if self.website in data:
-            username = data[self.website]['username']
-            password = data[self.website]['password']
-            messagebox.showinfo(title=self.website, message=f'Email: {username}\nPassword: {password}')
-        else:
-            messagebox.showinfo(title='No data found', message=f'No data for {self.website} exist.')
+        login_entry.insert(0, cred['email'])
+        pwd_entry.insert(0, cred['password'])
+
+def prepare_cred():
+    global login_entry, pwd_entry
+    username = get_value(login_entry)
+    pwd = get_value(pwd_entry)
+    cred_to_write = {
+        "email": username,
+        "password": pwd
+    }
+    return cred_to_write
+
+def prepare_param():
+    global company_entry, job_title_entry
+    company = get_value(company_entry)
+    title = get_value(job_title_entry)
+    split_company = company.split()
+    split_title = title.split()
+    params_to_write = {
+        "search_list": split_company,
+        "job_titles":  split_title,
+    }
+
+def save_cred():
+    if empty_cred_field_check():
+        data = prepare_cred()
+        username = data['email']
+        pwd = data['password']
+        with open('data/credentials.py', 'w', encoding='utf8') as outfile:
+            outfile.write(f'email = "{username}"\n')
+            outfile.write(f'password = "{pwd}"\n')
+
+def save_params():
+    if empty_param_field_check():
+        data = prepare_param()
+        company_list = data['search_list']
+        job_titles = data['job_titles']
+        with open('data/config.py', 'w', encoding='utf8') as outfile:
+            outfile.write(f'search_list: [{company_list}]\n')
+            # outfile.write(f'password = "{pwd}"\n')
+
+def get_params():
+    try:
+        with open('data/config.py', 'r', encoding='utf8') as outfile:
+            data = {
+            "search_list": config.search_list,
+            "job_titles": config.job_titles,
+            }
+        return data
+    except FileNotFoundError:
+        with open('data/config.py', 'w', encoding='utf8') as outfile:
+            outfile.write('search_list = "[]"\n')
+            outfile.write('job_title = "[]"\n')
+            data = {
+                "search_list": config.search_list,
+                "job_titles": config.job_titles,
+            }
+            return data
+    finally:
+        pass
+
+def predefine_params():
+    global job_title_entry, company_entry
+    """
+    Check if data file with login and password exist and populating values at the application start.
+    If file doesn't exist (first run) - returning None
+    :return:            None
+    """
+    data = get_params()
+    if len(data['search_list']) == 0 and len(data['job_titles']) == 0:
+        company_entry.insert(0, 'name the company')
+        job_title_entry(0, 'name the position title')
+    else:
+        company_entry.insert(0, data['search_list'])
+        job_title_entry.insert(0, data['job_titles'])
 # ==================== Console operations ====================
 
 # ==================== Building GUI ====================
-# def flip_background():
+def create_entry():
+    global login_entry, pwd_entry, job_title_entry, company_entry
+    # Creating text boxes
+    login_entry = Entry(width=25, justify='left', fg=text_blue, highlightthickness=0)
+    login_entry.grid(column=1, row=1)
+    login_entry.focus()
+    pwd_entry = Entry(width=25, justify='left', fg=text_blue, highlightthickness=0)
+    pwd_entry.grid(column=1, row=2)
+    job_title_entry = Entry(width=25, justify='left', fg=text_blue, highlightthickness=0)
+    job_title_entry.grid(column=1, row=3)
+    company_entry = Entry(width=25, justify='left', fg=text_blue, highlightthickness=0)
+    company_entry.grid(column=1, row=4)
 
+def create_labels():
+    login_txt = Label(text='       Login', font=('Arial', 15), bg="white", fg=text_blue)
+    login_txt.grid(column=0, row=1)
+    pwd_txt = Label(text='Password', font=('Arial', 15), bg="white", fg=text_blue, anchor='e')
+    pwd_txt.grid(column=0, row=2)
+    job_title_txt = Label(text='   Job title', font=('Arial', 15), bg="white", fg=text_blue, anchor='e')
+    job_title_txt.grid(column=0, row=3)
+    company_txt = Label(text='Company', font=('Arial', 15), bg="white", fg=text_blue, anchor='e')
+    company_txt.grid(column=0, row=4)
+    hint_txt = Label(text='* Job title and Company could have multiple values, separated by comma.', font=('Arial', 10),
+                     bg="white", fg=text_blue, anchor='e')
+    hint_txt.grid(column=0, row=5, columnspan=2)
+
+def create_buttons():
+    global clear_btn_img, save_btn_img, connect_btn_img
+    clear_btn_img = PhotoImage(file='data/img/clear_btn_img.png')
+    clear_login_btn = Button(text='', borderwidth=0, border=0, image=clear_btn_img, highlightthickness=0,
+                             command=clear_cred)
+    clear_login_btn.grid(column=2, row=1)
+    save_btn_img = PhotoImage(file='data/img/save_btn_img.png')
+    save_login_btn = Button(text='', borderwidth=0, border=0, image=save_btn_img, highlightthickness=0,
+                            command=save_cred)
+    save_login_btn.grid(column=2, row=2)
+    connect_btn_img = PhotoImage(file='data/img/connect_btn_img.png')
+    connect_btn = Button(text='', borderwidth=0, border=0, image=connect_btn_img, highlightthickness=0,
+                         command=clear_cred)
+    connect_btn.grid(column=1, row=6)
 
 window = Tk()
 window.title('LinkedIn autoconnect')
@@ -91,41 +256,11 @@ canvas_image = canvas.create_image(248, 70, image=main_bg)
 canvas.config(bg=white, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=3)
 
-# Creating text (labels)
-login_txt = Label(text='       Login', font=('Arial', 15), bg="white", fg=text_blue)
-login_txt.grid(column=0, row=1)
-pwd_txt = Label(text='Password', font=('Arial', 15), bg="white", fg=text_blue, anchor='e')
-pwd_txt.grid(column=0, row=2)
-job_title_txt = Label(text='   Job title', font=('Arial', 15), bg="white", fg=text_blue, anchor='e')
-job_title_txt.grid(column=0, row=3)
-company_txt = Label(text='Company', font=('Arial', 15), bg="white", fg=text_blue, anchor='e')
-company_txt.grid(column=0, row=4)
-hint_txt = Label(text='* Job title and Company could have multiple values, separated by comma.', font=('Arial', 10), bg="white", fg=text_blue, anchor='e')
-hint_txt.grid(column=0, row=5, columnspan=2)
+create_labels()
+create_entry()
+create_buttons()
 
-# Creating text boxes
-login_entry = Entry(width=25, justify='left', fg=text_blue, highlightthickness=0)
-login_entry.grid(column=1, row=1)
-login_entry.focus()
-pwd_entry = Entry(width=25, justify='left', fg=text_blue, highlightthickness=0)
-pwd_entry.grid(column=1, row=2)
-job_title_entry = Entry(width=25, justify='left', fg=text_blue, highlightthickness=0)
-job_title_entry.grid(column=1, row=3)
-company_entry = Entry(width=25, justify='left', fg=text_blue, highlightthickness=0)
-company_entry.grid(column=1, row=4)
-
-
-# Creating buttons
-clear_btn_img=PhotoImage(file='data/img/clear_btn_img.png')
-clear_login_btn = Button(text='', borderwidth=0, border=0, image=clear_btn_img, highlightthickness=0, command=clear_cred)
-clear_login_btn.grid(column=2, row=3)
-save_btn_img=PhotoImage(file='data/img/save_btn_img.png')
-save_login_btn = Button(text='', borderwidth=0, border=0, image=save_btn_img, highlightthickness=0, command=clear_cred)
-save_login_btn.grid(column=2, row=4)
-connect_btn_img=PhotoImage(file='data/img/connect_btn_img.png')
-connect_btn = Button(text='', borderwidth=0, border=0, image=connect_btn_img, highlightthickness=0, command=clear_cred)
-connect_btn.grid(column=1, row=6)
-
-
+predefine_cred()
+predefine_params()
 
 window.mainloop()
